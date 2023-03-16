30818347da6582aed79b449de7b7286ec28566d8
tuyizereerneste <tuyizereernest@yahoo.com>
2023-03-16 07:46:12 -0700

    puts name

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is main function
+ *
+ * Return: return 0 when succussful 
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
