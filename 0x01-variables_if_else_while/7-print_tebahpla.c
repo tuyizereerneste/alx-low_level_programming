d579ce0ce73100033f1a23166487ecdb58e80e49
tuyizereerneste <tuyizereernest@yahoo.com>
2023-03-17 11:21:19 -0700

    alphabet in descending

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: success return 0
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'z' ; n >= 'a' ; n--)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
+
+
