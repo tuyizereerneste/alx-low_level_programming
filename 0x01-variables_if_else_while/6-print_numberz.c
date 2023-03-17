efff66e3782a2661b707b43afcef84cd7b97f587
tuyizereerneste <tuyizereernest@yahoo.com>
2023-03-17 11:08:32 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: return 0 for success
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+		putchar(n + '0');
+	putchar('\n');
+	return (0);
+}
