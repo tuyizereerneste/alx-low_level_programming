2926f0035f2f065a96457e011e520efd41e1a38e
tuyizereerneste <tuyizereernest@yahoo.com>
2023-03-21 07:30:28 -0700

    print the word putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - the main function
+ * Description - function that prints '_putchat'
+ * Return: 0 when success
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
