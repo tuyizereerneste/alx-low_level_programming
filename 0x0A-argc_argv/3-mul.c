0cc55035408cc591c185c775777571545c049e0c
tuyizereerneste <tuyizereernest@yahoo.com>
2023-04-10 07:40:53 -0700

    Multiply two arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,16 +13,15 @@ int main(int argc, char *argv[])
 {
 	int n1, n2, mul = 1;
 
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return 1;
-    }
+	if (argc != 3)
+	{
+	printf("Error\n");
+	return (1);
+	}
 
-    n1 = atoi(argv[1]);
-    n2 = atoi(argv[2]);
-    mul = n1 * n2;
-
-    printf("%d\n", mul);
-    return 0;
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	mul = n1 * n2;
+	printf("%d\n", mul);
+	return (0);
 }
