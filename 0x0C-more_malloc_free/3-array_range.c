79f390e9ea3a013f4d946a470fa462f1fe720a20
tuyizereerneste <tuyizereernest@yahoo.com>
2023-04-14 15:41:55 -0700

    m inand max in malloc

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers.
+ * @min: minimum value.
+ * @max: maximum value.
+ *
+ * Return: pointer to the newly created array.
+ * if man > mix, returns NULL.
+ * if malloc fails, returns NULL.
+ */
+
+int *array_range(int min, int max)
+{
+	int *array;
+
+	int i;
+
+	if (min > max)
+	return (NULL);
+	array = malloc(sizeof(*array) * ((max - min) + 1));
+	if (array == NULL)
+	return (NULL);
+	for (i = 0; min <= max; i++, min++)
+	array[i] = min;
+	return (array);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
 
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+int *array_range(int min, int max);
+
 #endif
