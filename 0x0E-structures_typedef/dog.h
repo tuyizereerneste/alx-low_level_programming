2ae01e72992bc34a9e6945829dd2787c822cbd6b
tuyizereerneste <tuyizereernest@yahoo.com>
2023-04-17 16:33:44 -0700

    dog header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - a dog's basic info
+* @name: First element
+* @age: Second element
+* @owner: Third element
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
