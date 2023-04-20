5dd502bf878eb8448f2e2e623ae2f3242b8277d2
tuyizereerneste <tuyizereernest@yahoo.com>
2023-04-20 08:55:33 -0700

    print anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -34,8 +34,7 @@ void print_all(const char * const format, ...)
 	case 's':
 	str = va_arg(any, char *);
 	if (str == NULL)
-	printf("(nil)");
-	else
+	str = "(nil)";
 	printf("%s%s", sep, str);
 	break;
 	default:
