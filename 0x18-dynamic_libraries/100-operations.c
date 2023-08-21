9cd2561090f6b1ffc4e13fc4858431ac44850064
tuyizereerneste <tuyizereernest@yahoo.com>
2023-08-21 17:28:04 +0200

    C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,62 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - Function that add two numbers
+ * @a: first number
+ * @b: second number
+ * Return: always 0 when success
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Function that substract two numbers
+ * @a: first number
+ * @b: second number
+ * Return: always 0 when success
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * multiply - Function that multiplies two numbers
+ *  @a: first number
+ *  @b: second number
+ *  Return: always 0 when success
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * divide - Function that divides two numbers
+ * @a: first number
+ * @b: second number
+ * Return: always 0 when success
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * modulus - Function that find modulus two numbers
+ * @a: first number
+ * @b: second number
+ * Return: always 0 when success
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
